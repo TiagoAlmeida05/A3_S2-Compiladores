c2aa3f953f91d2d6c35f643c53d51baada4761c8
carolinaroquee <carolina.roque.005@gmail.com>
2026-04-27 16:37:35 +0100

    8 optimizations and 7 statements tests left to pass

--- src/main/pt/up/fe/comp2026/optimization/JmmOptimizationImpl.java
+++ src/main/pt/up/fe/comp2026/optimization/JmmOptimizationImpl.java
@@ -1,11 +1,15 @@
 package pt.up.fe.comp2026.optimization;
 
+
+import org.specs.comp.ollir.Method;
+import org.specs.comp.ollir.Operand;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
 
-
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 public class JmmOptimizationImpl implements JmmOptimization {
 
@@ -33,14 +37,61 @@ public class JmmOptimizationImpl implements JmmOptimization {
     @Override
     public OllirResult transformOllir(OllirResult ollirResult) {
 
-        if (ollirResult.config().getOrDefault("debug", "false").equals("true")) {
-            System.out.println("OLLIR CODE:");
-            System.out.println(ollirResult.getOllirCode());
+        var config = ollirResult.config();
+        int r = Integer.parseInt(config.getOrDefault("registerAllocation", "-1"));
+
+        if (r == -1) {
+            return ollirResult;
         }
 
-        //TODO: Do your OLLIR-based optimizations here
+        var classUnit = ollirResult.getOllirClass();
+
+        for (var method : classUnit.getMethods()) {
+            if (method.isConstructMethod()) continue;
+
+            allocateRegisters(method, r);
+        }
 
         return ollirResult;
+    }
+
+    private void allocateRegisters(Method method, int r) {
+
+        var varTable = method.getVarTable();
+
+        boolean isStatic = method.isStaticMethod();
+        int nextReg = isStatic ? 0 : 1;
+
+        Map<String, Integer> regMap = new java.util.HashMap<>();
+        List<Integer> freeRegs = new java.util.ArrayList<>();
+
+        for (var param : method.getParams()) {
+            if (param instanceof Operand op) {
+                int reg = nextReg++;
+                varTable.get(op.getName()).setVirtualReg(reg);
+                regMap.put(op.getName(), reg);
+            }
+        }
+
+        for (var entry : varTable.entrySet()) {
+
+            var name = entry.getKey();
+            if (name.equals("this")) continue;
+            if (regMap.containsKey(name)) continue;
+
+            int reg;
+
+            if (!freeRegs.isEmpty()) {
+                reg = freeRegs.remove(0);
+            } else {
+                reg = nextReg++;
+            }
+
+            entry.getValue().setVirtualReg(reg);
+            regMap.put(name, reg);
+
+            freeRegs.add(reg);
+        }
     }
 
 }
